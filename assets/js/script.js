3d7c36520bfe646bcd33e388cb837e7d9154777f
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-05-24 10:19:18 +0000

    java canvas filled

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 340;
+canvas.height = 600;
+
+function gameLoop() {}
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+setInterval(gameLoop, 1000/60);
